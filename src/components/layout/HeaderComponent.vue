973fe68b561ffca3e10bb6995cd369336837068f
Беляков Андрей <andreybel.20033@gmail.com>
2025-08-18 10:22:27 +0300

    Добавлена возможность открытия и закрытия меню в мобильной версии

--- src/components/layout/HeaderComponent.vue
+++ src/components/layout/HeaderComponent.vue
@@ -2,6 +2,16 @@
 export default {
   name: 'HeaderComponent'
 }
+document.addEventListener("DOMContentLoaded", () => {
+    const menuTrigger = document.querySelector('[data-mobile-menu-trigger]');
+    const lowerheader = document.getElementById("lower-header");
+
+    menuTrigger.addEventListener("click", () => {
+        if (window.innerWidth <= 768) {
+            lowerheader.classList.toggle("open");
+        }
+    });
+});
 </script>
 
 <template>
